11206c0b7f543d32614acd9290e6ae1565e4d969
ustav <gustavobreasy@gmail.com>
2024-02-11 19:22:23 -0300

    fix: conditional rendering pinned repos title

--- src/pages/PinnedRepos.tsx
+++ src/pages/PinnedRepos.tsx
@@ -7,42 +7,53 @@ import { Link } from "react-router-dom";
 
 
 
-export function PinnedRepos() {
 
+export function PinnedRepos() {
     const { data: pinnedRepos, isFetching: isPinnedFetching } = useQuery({
-        queryKey: ['pinned'], queryFn: getQlRepositories, staleTime: 60000, refetchOnMount: 'always',
-    })
+        queryKey: ['pinned'],
+        queryFn: getQlRepositories,
+        staleTime: 60000,
+        refetchOnMount: 'always',
+    });
 
     return (
-        <>
-            <h1 className='text-2xl text-center py-10'>Pinned Repositories:</h1>
-            <div className='flex  justify-center gap-10 flex-row min-w-54 flex-wrap'>
-                {isPinnedFetching ?
+        <div>
+            {isPinnedFetching ? (
+                <>
+                    <h1 className='text-2xl text-center py-10'>Pinned Repositories:</h1>
                     <div className='flex justify-center gap-10 flex-row min-w-54 flex-wrap'>
-                        <Skeleton className='w-[15rem] h-[20rem] ' />
                         <Skeleton className='w-[15rem] h-[20rem]' />
-                        <Skeleton className='w-[15rem] h-[20rem] ' />
-                        <Skeleton className='w-[15rem] h-[20rem] ' />
+                        <Skeleton className='w-[15rem] h-[20rem]' />
+                        <Skeleton className='w-[15rem] h-[20rem]' />
+                        <Skeleton className='w-[15rem] h-[20rem]' />
                     </div>
-                    :
-                    pinnedRepos && pinnedRepos.data.user.pinnedItems.edges.map(e => {
-                        return (
+                </>
+            ) : (
+                pinnedRepos &&
+                pinnedRepos.data.user.pinnedItems.totalCount > 0 &&
+                <>
+                    <h1 className='block text-2xl text-center py-10'>Pinned Repositories:</h1>
+                    <div className='flex justify-center gap-10 flex-row min-w-54 flex-wrap'>
+                        {pinnedRepos.data.user.pinnedItems.edges.map(e => (
                             <Card key={e.node.name} className='w-[15rem] h-[20rem] text-center flex flex-col justify-center'>
                                 <CardHeader>
-                                    <CardTitle >{e.node.name}</CardTitle>
+                                    <CardTitle>{e.node.name}</CardTitle>
                                     <CardDescription>{e.node.nameWithOwner}</CardDescription>
                                 </CardHeader>
                                 <CardContent>
                                     {e.node.description}
                                 </CardContent>
                                 <CardFooter>
-                                    <Link className='m-auto' to={`/repos/${e.node.nameWithOwner}`}><Button>Details</Button></Link>
+                                    <Link className='m-auto' to={`/repos/${e.node.nameWithOwner}`}>
+                                        <Button>Details</Button>
+                                    </Link>
                                 </CardFooter>
                             </Card>
-                        )
-                    })
-                }
-            </div>
-        </>
-    )
+                        ))}
+                    </div>
+                </>
+
+            )}
+        </div>
+    );
 }
