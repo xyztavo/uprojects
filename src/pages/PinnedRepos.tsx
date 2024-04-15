24e00a6f0eff09bff6859ede38c2d7e9a7f71e65
ustav <gustavobreasy@gmail.com>
2024-04-15 20:38:36 -0300

    make vercel stop bitching

--- src/pages/PinnedRepos.tsx
+++ src/pages/PinnedRepos.tsx
@@ -16,28 +16,29 @@ export function PinnedRepos() {
     });
 
     return (
-                pinnedRepos &&
-                pinnedRepos.data.user.pinnedItems.totalCount > 0 &&
-                <div className="p-4">
-                    <h1 className='block text-2xl text-center py-10'>Pinned Repositories:</h1>
-                    <div className='flex justify-center gap-10 flex-row min-w-54 flex-wrap'>
-                        {pinnedRepos.data.user.pinnedItems.edges.map(e => (
-                            <Card key={e.node.name} className='w-[15rem] h-[20rem] text-center flex flex-col justify-center'>
-                                <CardHeader>
-                                    <CardTitle>{e.node.name}</CardTitle>
-                                    <CardDescription>{e.node.nameWithOwner}</CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    {e.node.description}
-                                </CardContent>
-                                <CardFooter>
-                                    <Link className='m-auto' to={`/repos/${e.node.nameWithOwner}`}>
-                                        <Button>Details</Button>
-                                    </Link>
-                                </CardFooter>
-                            </Card>
-                        ))}
-                    </div>
+            pinnedRepos &&
+            pinnedRepos.data.user.pinnedItems.totalCount > 0 &&
+            <div className="p-4">
+                <h1 className='block text-2xl text-center py-10'>Pinned Repositories:</h1>
+                <div className='flex justify-center gap-10 flex-row min-w-54 flex-wrap'>
+                    {pinnedRepos.data.user.pinnedItems.edges.map(e => (
+                        <Card key={e.node.name} className='w-[15rem] h-[20rem] text-center flex flex-col justify-center'>
+                            <CardHeader>
+                                <CardTitle>{e.node.name}</CardTitle>
+                                <CardDescription>{e.node.nameWithOwner}</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                {e.node.description}
+                            </CardContent>
+                            <CardFooter>
+                                <Link className='m-auto' to={`/repos/${e.node.nameWithOwner}`}>
+                                    <Button>Details</Button>
+                                </Link>
+                            </CardFooter>
+                        </Card>
+                    ))}
                 </div>
+            </div>
+
     );
 }
