f38a9c303c71ad7a0919cc0bdd8dcb1fbdd8e6b0
torvalds <torvalds@linux-foundation.org>
2025-08-25 20:16:20 -0300

    fix(github): get pinned repositories

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -5,7 +5,7 @@ type IGetPinnedRepository = Omit<IPinnedRepository, "nameWithOwner">;
 export const getPinnedRepositories = async (githubUser: string) => {
     const response = await fetch(`https://pinned.berrysauce.dev/get/${githubUser}`);
 
-    if (response.ok) {
+    if (!response.ok) {
         throw new Error("Failed to get pinned repositories");
     }
 
